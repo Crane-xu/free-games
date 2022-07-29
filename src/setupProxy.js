4ad0228abd5f20f5744eb30df017bab354bf9b2c
徐海辉 <xuhaihui@jszzkj.cn>
2022-07-29 17:42:15 +0800

    清楚注释

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,21 +3,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use('/api', createProxyMiddleware({
         target: 'https://www.freetogame.com',
-        // target: 'http://localhost:85',
         changeOrigin: true,
         pathRewrite: {
             '^/api': '/api',
         },
     }));
-
-    // app.use(
-    //     '/g',
-    //     createProxyMiddleware({
-    //         target: 'https://www.freetogame.com',
-    //         changeOrigin: true,
-    //         pathRewrite: {
-    //             '^/g': '/g',
-    //         }
-    //     })
-    // );
 };
